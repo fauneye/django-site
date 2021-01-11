816d3deb5fb4dec8bdb6eec2513d3e28a98c1721
fauneye <52381990+fauneye@users.noreply.github.com>
2021-01-12 02:37:24 +0300

    Update views.py

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from .models import Task
 from .forms import TaskForm
 
 def index(request):
-    tasks = Task.objects.order_by('-id')[:1]
+    tasks = Task.objects.order_by('-id')
     return render(request, 'main/index.html', {'title': 'Главная', 'tasks': tasks})
 
 
